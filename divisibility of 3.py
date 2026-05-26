e43ab58338cbee792d631b30338e4a2cf57f1c39
namansharma2707 <sh.naman2707@gmail.com>
2026-05-26 14:03:20 +0530

    Add files via upload

---
+++ divisibility of 3.py
@@ -0,0 +1,6 @@
+def oe(n):
+    return n%3
+if oe(n=int(input("enter the no:"))):
+    print("not divisible")
+else:
+    print("divisible")
---
+++ odd_even.py
@@ -0,0 +1,6 @@
+def oe(n):
+    return n%2
+if oe(n=int(input("enter the no:"))):
+    print("odd")
+else:
+    print("even")
